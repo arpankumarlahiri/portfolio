9ea9a01376f40c0c6f9a37a0bf7c265d61d2206f
Arpan Lahiri <arpanlahiri@plexusmd.com>
2023-07-06 01:13:08 +0530

    minor css fix

--- src/components/Navbar/RightComponent/RightContent.tsx
+++ src/components/Navbar/RightComponent/RightContent.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AuthButton from "./AuthButton";
+import { Flex } from "@chakra-ui/react";
 
 const RightContent: React.FC = () => {
   return (
-    <>
+    <Flex align={"center"}>
       <AuthButton />
-    </>
+    </Flex>
   );
 };
 export default RightContent;
